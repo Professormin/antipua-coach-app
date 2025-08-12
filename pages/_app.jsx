eecb74a9f12d891ce347a1ab803f2b551463a2ee
Professor_Z <zhenghuiminer@gmail.com>
2025-08-12 22:16:02 +0800

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,13 @@
 import '../styles/globals.css';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* 探针样式：部署后如果你看到页面四周出现紫色边框，说明全局样式渲染链路OK */}
+      <style jsx global>{`
+        body { outline: 4px solid rebeccapurple; }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
